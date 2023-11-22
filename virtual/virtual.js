9e26f05fe6fb3b709bb3435ba53ef7378a433058
Rockstar1215 <vikram.poojary1503@gmail.com>
2023-11-22 13:23:00 +0530

    second commit

---
+++ virtual/virtual.js
@@ -0,0 +1,14 @@
+gsap.to("nav", {
+    backgroundColor: "#000", // Set the initial background color to black
+    duration: 0.5,
+    scrollTrigger: {
+      trigger: "body",
+      start: "top -10%",
+      end: "top -11%",
+      scrub: 1,
+      onUpdate: ({ progress }) => {
+        const backgroundColor = progress > 0 ? "#fff" : "#fff";
+        gsap.set("nav", { backgroundColor });
+      },
+    },
+  });
